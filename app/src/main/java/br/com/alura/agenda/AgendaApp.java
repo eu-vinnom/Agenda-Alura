db0ef064f03648e6c482093d13a4f99c017caafe
eu-vinnom <vinnom.2d@gmail.com>
2020-01-25 02:36:33 -0300

    adição de um comportamento personalizado à camada Application

---
+++ app/src/main/java/br/com/alura/agenda/AgendaApp.java
@@ -0,0 +1,21 @@
+package br.com.alura.agenda;
+
+import android.app.Application;
+
+import br.com.alura.agenda.dao.AlunoDao;
+import br.com.alura.agenda.model.Aluno;
+
+public class AgendaApp extends Application{
+
+	@Override
+	public void onCreate(){
+		super.onCreate();
+		criaAlunosParaTeste();
+	}
+
+	private void criaAlunosParaTeste(){
+		AlunoDao alunoDao = new AlunoDao();
+		alunoDao.salva(new Aluno("vi", "123456789", "vi@vi.com.br"));
+		alunoDao.salva(new Aluno("ka", "123456789", "ka@ka.com.br"));
+	}
+}
